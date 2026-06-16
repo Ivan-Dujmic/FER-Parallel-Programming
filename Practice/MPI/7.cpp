c6a5f5af5e9f93ad39a76b4704a4a352961d7db5
Ivan-Dujmic <ivan.dujmic.sb@gmail.com>
2026-06-16 14:16:34 +0200

    Practice MPI 7 (perfect hypercube)

---
+++ Practice/MPI/7.cpp
@@ -0,0 +1,108 @@
+/*
+U MPI programu u nekom trenutku svih N procesa treba obaviti kritični odsječak.
+Svaki proces zna svoj redni broj ulaska u K.O.,
+ali ne zna redne brojeve ostalih procesa.
+Korištenjem MPI funkcija Send i Recv
+napisati odsječak programa logaritamske složenosti (po pitanju broja poslanih poruka)
+koji će omogućiti da svaki proces sazna indeks svog neposrednog prethodnika i sljedbenika.
+*/
+
+/*
+In an MPI program, at some point all N processes must execute a critical section.
+Each process knows its ordinal number of entry into the critical section,
+but does not know the ordinal numbers of the other processes.
+Using the MPI functions Send and Recv,
+write a program segment with logarithmic complexity (with respect to the number of sent messages)
+that will allow each process to determine the index of its immediate predecessor and successor.
+*/
+
+#include <mpi.h>
+#include <iostream>
+#include <sstream>
+#include <random>
+#include <algorithm>
+#include <vector>
+
+/*
+This hypercube solution works when the number of processes is a power of 2.
+
+Only the processes with ranks that are submasks of world_size-1 have full info
+    Submask here means that ranks don't have set bits where world_size-1 has doesn't have set bits
+*/
+
+int main()  {
+    MPI_Init(nullptr, nullptr);
+
+    int world_size;
+    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+
+    int rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    int ticket;
+
+    // Setup
+    if (rank == 0) {
+        std::vector<int> tickets;
+        tickets.reserve(world_size);
+        for (int i = 0 ; i < world_size ; i++) {
+            tickets.push_back(i);
+        }
+
+        std::mt19937 rng(std::random_device{}());
+        std::shuffle(tickets.begin(), tickets.end(), rng);
+
+        std::cout << "Generated tickets:";
+        for (int t : tickets) {
+            std::cout << ' ' << t;
+        }
+        std::cout << "\n\n";
+
+        ticket = tickets[0];
+
+        for (int i = 1 ; i < world_size ; i++) {
+            MPI_Send(&tickets[i], 1, MPI_INT, i, 0, MPI_COMM_WORLD);
+        }
+    } else {
+        MPI_Recv(&ticket, 1, MPI_INT, 0, MPI_ANY_TAG, MPI_COMM_WORLD, nullptr);
+    }
+
+    std::vector<int> tickets(world_size, -1);
+    tickets[rank] = ticket;
+
+    std::vector<int> other_tickets(world_size, 0);
+
+    for (int mask = 1 ; mask < world_size ; mask <<= 1) {
+        int partner = rank ^ mask;
+        if (partner < world_size) {
+            if (rank < partner) { // Receive then send
+                MPI_Recv(other_tickets.data(), world_size, MPI_INT, partner, MPI_ANY_TAG, MPI_COMM_WORLD, nullptr);
+                for (int i = 0 ; i < world_size ; i++) { // Update with new info
+                    if (tickets[i] != -1) continue; // I already know that
+                    tickets[i] = other_tickets[i];
+                }
+                MPI_Send(tickets.data(), world_size, MPI_INT, partner, 0, MPI_COMM_WORLD);
+            } else { // Send then receive
+                MPI_Send(tickets.data(), world_size, MPI_INT, partner, 0, MPI_COMM_WORLD);
+                MPI_Recv(other_tickets.data(), world_size, MPI_INT, partner, MPI_ANY_TAG, MPI_COMM_WORLD, nullptr);
+                for (int i = 0 ; i < world_size ; i++) { // Update with new info
+                    if (tickets[i] != -1) continue; // I already know that
+                    tickets[i] = other_tickets[i];
+                }
+            }
+        }
+    }
+
+    // TODO: imperfect hypercube
+
+    std::ostringstream msg;
+    msg << rank << " knows:";
+    for (int t : tickets) {
+        msg << ' ' << t;
+    }
+    msg << '\n';
+    std::cout << msg.str();
+
+    MPI_Finalize();
+    return 0;
+}
